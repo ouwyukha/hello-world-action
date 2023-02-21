9c1c1ae44dc441c9a00749811595c8dd3870a3bb
Yukha Dharmeswara <yukha.dw@samsung.com>
2023-02-21 10:36:38 +0700

    add write delete

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+const fs = require('fs');
 console.log('main');
+fs.mkdirSync('~/data', { recursive: true });
+fs.writeFileSync('~/data/data', 'data');
+const data = fs.readFileSync('~/data/data');
+console.log(data.toString());
--- post.js
+++ post.js
@@ -1,1 +1,3 @@
+const fs = require('fs');
 console.log('post');
+fs.rmSync('~/data', { recursive: true, force: true });
